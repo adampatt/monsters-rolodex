b66ecf9975a0cb6ec51c3a85c6f22e22fd72d8e5
adampatt <adampattison08@gmail.com>
2020-12-11 15:50:03 +0000

    28 Fetching content, pt1

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [
+        {
+          name: 'Frankenstein',
+          id: 'ss'
+        },
+        {
+          name: 'Dracula',
+          id: 'aa'
+        },
+        {
+          name: 'Zombie',
+          id: 'bb'
+        }
+      ]
+    };
+  }
+  
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then (users => this.setState({ monsters: users});
+  }
+
+render () {
+  return (
+    <div className="App">
+      {
+        this.state.monsters.map(monster => (
+          <h1 key={monster.id}> {monster.name} </h1>
+          
+        ))
+      }   
+    </div>
+  );
+}
+
+}
+
+export default App;
