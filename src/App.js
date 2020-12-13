7b15691b140bd8e6355f4a39316d80099afd46c4
adampatt <adampattison08@gmail.com>
2020-12-13 12:33:44 +0000

    class components and methods using this se arrow functions

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
-
 import { CardList } from './components/card-list/card-list.component';
-
 import './App.css';
-
 import { SearchBox } from './components/search-box/search-box.component';
 
 class App extends Component {
@@ -11,32 +8,36 @@ class App extends Component {
     super();
     this.state = {
       monsters: [],
-      searchField: ''
+      searchField: "",
     };
   }
-  
-  componentDidMount(){
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then (users => this.setState({ monsters: users}));
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
   }
 
-render () {
-  const { monsters, searchField } = this.state;
-  const filteredMonsters = monsters.filter(monster => 
-    monster.name.toLowerCase().includes(searchField.toLowerCase())
-    )
-  return (
-    <div className="App">
-      <SearchBox
-        placeholder="search monsters"
-        handleChange={(e) => this.setState({ searchField: e.target.value })}
-      />
-      <CardList monsters={filteredMonsters} />
-    </div>
-  );
-}
+  handleChange = e => {
+    this.setState({ searchField: e.target.value });
+  }
 
+  render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
+    return (
+      <div className="App">
+        <SearchBox
+          placeholder="search monsters"
+          handleChange={(e) => this.setState({ searchField: e.target.value })}
+        />
+        <CardList monsters={filteredMonsters} />
+      </div>
+    );
+  }
 }
 
 export default App;
